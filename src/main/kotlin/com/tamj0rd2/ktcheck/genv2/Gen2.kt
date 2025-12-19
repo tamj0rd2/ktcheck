a9a8e2fb819a46d78dc7b02e6efad72758044234
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-12-19 21:00:34 +0000

    Cleaning up

--- src/main/kotlin/com/tamj0rd2/ktcheck/genv2/Gen2.kt
+++ src/main/kotlin/com/tamj0rd2/ktcheck/genv2/Gen2.kt
@@ -1,17 +1,19 @@
 package com.tamj0rd2.ktcheck.genv2
 
+data class GenResult<T>(val value: T, val shrinks: Sequence<SampleTree>)
+
 fun interface Gen2<T> {
-    fun generate(tree: SampleTree): Pair<T, Sequence<SampleTree>>
+    fun generate(tree: SampleTree): GenResult<T>
 
     fun <R> map(fn: (T) -> R): Gen2<R> = Gen2 { tree ->
         val (value, shrinks) = generate(tree)
-        fn(value) to shrinks
+        GenResult(fn(value), shrinks)
     }
 
     fun <R> flatMap(fn: (T) -> Gen2<R>): Gen2<R> = Gen2 { tree ->
         val (leftValue, leftShrinks) = generate(tree.left)
         val (rightValue, rightShrinks) = fn(leftValue).generate(tree.right)
-        rightValue to combineShrinks(tree, leftShrinks, rightShrinks)
+        GenResult(rightValue, combineShrinks(tree, leftShrinks, rightShrinks))
     }
 
     fun list(size: IntRange = 0..100): Gen2<List<T>> = Gen2.int(size).flatMap(::list)
@@ -28,25 +30,19 @@ fun interface Gen2<T> {
 
 fun <T> Gen2.Companion.constant(value: T): Gen2<T> = sample().map { value }
 
-fun interface Shrinker<T> {
-    operator fun invoke(value: T): Sequence<T>
-}
-
-private val uLongShrinker = Shrinker<ULong> { value ->
-    sequence {
-        if (value == 0UL) return@sequence
-        yield(0uL)
-
-        var current = value
-        while (current > 0UL) {
-            val diff = value - current
-            if (diff > 0uL) yield(diff)
-            current /= 2UL
-        }
-    }
-}
-
 internal fun Gen2.Companion.sample(): Gen2<Sample> = Gen2 { tree ->
-    val shrinks = uLongShrinker(tree.sample.value).map { tree.withSample(Sample.Shrunk(it)) }
-    tree.sample to shrinks
+    val shrinks = shrink(tree.sample.value).map { tree.withSample(Sample.Shrunk(it)) }
+    GenResult(tree.sample, shrinks)
+}
+
+private fun shrink(value: ULong) = sequence {
+    if (value == 0UL) return@sequence
+    yield(0uL)
+
+    var current = value
+    while (current > 0UL) {
+        val diff = value - current
+        if (diff > 0uL) yield(diff)
+        current /= 2UL
+    }
 }
