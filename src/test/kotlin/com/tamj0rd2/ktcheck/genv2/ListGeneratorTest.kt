08ce814a0ae8a9632ec03679de26bfece54562d9
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-12-20 14:49:02 +0000

    Rename test

---
+++ src/test/kotlin/com/tamj0rd2/ktcheck/genv2/ListGeneratorTest.kt
@@ -0,0 +1,10 @@
+package com.tamj0rd2.ktcheck.genv2
+
+import org.junit.jupiter.api.Test
+
+class ListGeneratorTest {
+    @Test
+    fun `can generate a long list without stack overflow`() {
+        Gen.constant(1).list(10_000).sample()
+    }
+}
