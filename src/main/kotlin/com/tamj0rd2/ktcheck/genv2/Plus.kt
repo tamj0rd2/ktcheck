dc31a9f493ad200741056202ecc393015de614f7
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-12-20 15:43:00 +0000

    Extract function to reduce duplication

--- src/main/kotlin/com/tamj0rd2/ktcheck/genv2/Plus.kt
+++ src/main/kotlin/com/tamj0rd2/ktcheck/genv2/Plus.kt
@@ -14,106 +14,45 @@ import com.tamj0rd2.ktcheck.util.tuple
 
 @JvmName("zip2")
 infix operator fun <T1, T2> Gen<T1>.plus(
-    other: Gen<T2>,
-) = Gen<Tuple2<T1, T2>> { tree ->
-    val (leftValue, leftShrinks) = generate(tree.left)
-    val (rightValue, rightShrinks) = other.generate(tree.right)
-    val tuple = tuple(leftValue, rightValue)
-    GenResult(tuple, combineShrinks(tree, leftShrinks, rightShrinks))
-}
+    nextGen: Gen<T2>,
+) = combineWith(nextGen) { first, second -> tuple(first, second) }
 
 @JvmName("zip3")
 infix operator fun <T1, T2, T3> Gen<Tuple2<T1, T2>>.plus(
     nextGen: Gen<T3>,
-) = Gen { tree ->
-    val (tupleValue, tupleShrinks) = generate(tree.left)
-    val (nextValue, nextShrinks) = nextGen.generate(tree.right)
-    GenResult(
-        value = tupleValue + nextValue,
-        shrinks = combineShrinks(tree, tupleShrinks, nextShrinks)
-    )
-}
+) = combineWith(nextGen) { tuple, nextValue -> tuple + nextValue }
 
 @JvmName("zip4")
 infix operator fun <T1, T2, T3, T4> Gen<Tuple3<T1, T2, T3>>.plus(
     nextGen: Gen<T4>,
-) = Gen { tree ->
-    val (tupleValue, tupleShrinks) = generate(tree.left)
-    val (nextValue, nextShrinks) = nextGen.generate(tree.right)
-    GenResult(
-        value = tupleValue + nextValue,
-        shrinks = combineShrinks(tree, tupleShrinks, nextShrinks)
-    )
-}
+) = combineWith(nextGen) { tuple, nextValue -> tuple + nextValue }
 
 @JvmName("zip5")
 infix operator fun <T1, T2, T3, T4, T5> Gen<Tuple4<T1, T2, T3, T4>>.plus(
     nextGen: Gen<T5>,
-) = Gen { tree ->
-    val (tupleValue, tupleShrinks) = generate(tree.left)
-    val (nextValue, nextShrinks) = nextGen.generate(tree.right)
-    GenResult(
-        value = tupleValue + nextValue,
-        shrinks = combineShrinks(tree, tupleShrinks, nextShrinks)
-    )
-}
+) = combineWith(nextGen) { tuple, nextValue -> tuple + nextValue }
 
 @JvmName("zip6")
 infix operator fun <T1, T2, T3, T4, T5, T6> Gen<Tuple5<T1, T2, T3, T4, T5>>.plus(
     nextGen: Gen<T6>,
-) = Gen { tree ->
-    val (tupleValue, tupleShrinks) = generate(tree.left)
-    val (nextValue, nextShrinks) = nextGen.generate(tree.right)
-    GenResult(
-        value = tupleValue + nextValue,
-        shrinks = combineShrinks(tree, tupleShrinks, nextShrinks)
-    )
-}
+) = combineWith(nextGen) { tuple, nextValue -> tuple + nextValue }
 
 @JvmName("zip7")
 infix operator fun <T1, T2, T3, T4, T5, T6, T7> Gen<Tuple6<T1, T2, T3, T4, T5, T6>>.plus(
     nextGen: Gen<T6>,
-) = Gen { tree ->
-    val (tupleValue, tupleShrinks) = generate(tree.left)
-    val (nextValue, nextShrinks) = nextGen.generate(tree.right)
-    GenResult(
-        value = tupleValue + nextValue,
-        shrinks = combineShrinks(tree, tupleShrinks, nextShrinks)
-    )
-}
+) = combineWith(nextGen) { tuple, nextValue -> tuple + nextValue }
 
 @JvmName("zip8")
 infix operator fun <T1, T2, T3, T4, T5, T6, T7, T8> Gen<Tuple7<T1, T2, T3, T4, T5, T6, T7>>.plus(
     nextGen: Gen<T8>,
-) = Gen { tree ->
-    val (tupleValue, tupleShrinks) = generate(tree.left)
-    val (nextValue, nextShrinks) = nextGen.generate(tree.right)
-    GenResult(
-        value = tupleValue + nextValue,
-        shrinks = combineShrinks(tree, tupleShrinks, nextShrinks)
-    )
-}
+) = combineWith(nextGen) { tuple, nextValue -> tuple + nextValue }
 
 @JvmName("zip9")
 infix operator fun <T1, T2, T3, T4, T5, T6, T7, T8, T9> Gen<Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>>.plus(
     nextGen: Gen<T9>,
-) = Gen { tree ->
-    val (tupleValue, tupleShrinks) = generate(tree.left)
-    val (nextValue, nextShrinks) = nextGen.generate(tree.right)
-    GenResult(
-        value = tupleValue + nextValue,
-        shrinks = combineShrinks(tree, tupleShrinks, nextShrinks)
-    )
-}
+) = combineWith(nextGen) { tuple, nextValue -> tuple + nextValue }
 
 @JvmName("zip10")
 infix operator fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> Gen<Tuple9<T1, T2, T3, T4, T5, T6, T7, T8, T9>>.plus(
     nextGen: Gen<T10>,
-) = Gen { tree ->
-    val (tupleValue, tupleShrinks) = generate(tree.left)
-    val (nextValue, nextShrinks) = nextGen.generate(tree.right)
-    GenResult(
-        value = tupleValue + nextValue,
-        shrinks = combineShrinks(tree, tupleShrinks, nextShrinks)
-    )
-}
+) = combineWith(nextGen) { tuple, nextValue -> tuple + nextValue }
