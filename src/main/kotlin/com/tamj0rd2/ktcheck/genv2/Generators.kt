8cd833872a6f1ac41db9e026a92ad20d9874a9ed
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-12-20 15:16:52 +0000

    Fix copy paste error

--- src/main/kotlin/com/tamj0rd2/ktcheck/genv2/Generators.kt
+++ src/main/kotlin/com/tamj0rd2/ktcheck/genv2/Generators.kt
@@ -1,6 +1,7 @@
 package com.tamj0rd2.ktcheck.genv2
 
 import com.tamj0rd2.ktcheck.gen.OneOfEmpty
+import kotlin.reflect.KClass
 
 fun <T> Gen.Companion.constant(value: T): Gen<T> = sample().map { value }
 
@@ -9,6 +10,7 @@ fun <T> Gen.Companion.oneOf(gens: List<Gen<T>>): Gen<T> {
     return int(0..<gens.size).flatMap { gens[it] }
 }
 
+@Suppress("unused")
 class OneOfEmpty : IllegalStateException("Gen.oneOf() called with no generators")
 
 /** Shrinks toward the first value */
@@ -18,14 +20,21 @@ fun <T> Gen.Companion.of(values: Collection<T>): Gen<T> = Gen.oneOf(values.map(:
         /** Shrinks toward the smallest value */
 fun <T : Comparable<T>> Gen.Companion.of(values: Collection<T>): Gen<T> = Gen.oneOf(values.sorted().map(::constant))
 
+@Suppress("unused")
 fun <T> Gen<T>.filter(predicate: (T) -> Boolean): Gen<T> = filter(100, predicate)
+
+@Suppress("unused", "UnusedReceiverParameter")
 fun <T> Gen<T>.filter(threshold: Int, predicate: (T) -> Boolean): Gen<T> = TODO("Not yet implemented")
 
-class FilterLimitReached(threshold: Int, override val cause: Throwable) :
-    IllegalStateException("Gen.ignoreExceptions() exceeded the threshold of $threshold exceptions")
+@Suppress("unused")
+class FilterLimitReached(threshold: Int) :
+    IllegalStateException("Gen.filter() exceeded the threshold of misses")
 
-fun <T> Gen<T>.ignoreExceptions(klass: Class<out Throwable>, threshold: Int = 100): Gen<T> = TODO("Not yet implemented")
+@Suppress("unused", "UnusedReceiverParameter")
+fun <T> Gen<T>.ignoreExceptions(klass: KClass<out Throwable>, threshold: Int = 100): Gen<T> =
+    TODO("Not yet implemented")
 
+@Suppress("unused")
 class ExceptionLimitReached(threshold: Int, override val cause: Throwable) :
     IllegalStateException("Gen.ignoreExceptions() exceeded the threshold of $threshold exceptions")
 
