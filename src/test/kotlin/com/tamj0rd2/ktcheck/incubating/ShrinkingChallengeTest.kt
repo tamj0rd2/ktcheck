e18b6b1199ecbd9d8e96c83a1c0f15be1ae04315
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2026-02-21 23:04:09 +0000

    Enable the deletion test

--- src/test/kotlin/com/tamj0rd2/ktcheck/incubating/ShrinkingChallengeTest.kt
+++ src/test/kotlin/com/tamj0rd2/ktcheck/incubating/ShrinkingChallengeTest.kt
@@ -1,22 +1,21 @@
 package com.tamj0rd2.ktcheck.incubating
 
 import com.tamj0rd2.ktcheck.contracts.ShrinkingChallengeContract
-import org.junit.jupiter.api.Assumptions
+import org.junit.jupiter.api.Disabled
 
 internal class ShrinkingChallengeTest : BaseContractImpl(), ShrinkingChallengeContract {
-    override fun deletion() {
-        Assumptions.assumeTrue(false, "TODO: make this pass and delete this override")
-    }
-
+    @Disabled("TODO: make this pass and delete this override")
     override fun `difference must not be one`() {
-        Assumptions.assumeTrue(false, "TODO: make this pass and delete this override")
+        super.`difference must not be one`()
     }
 
+    @Disabled("TODO: make this pass and delete this override")
     override fun `difference must not be small`() {
-        Assumptions.assumeTrue(false, "TODO: make this pass and delete this override")
+        super.`difference must not be small`()
     }
 
+    @Disabled("TODO: make this pass and delete this override")
     override fun `difference must not be zero`() {
-        Assumptions.assumeTrue(false, "TODO: make this pass and delete this override")
+        super.`difference must not be zero`()
     }
 }
